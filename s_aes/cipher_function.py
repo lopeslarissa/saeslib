112720ec0a917e20a3d3ba51aa76a60dab8dcbb0
Larissa Lopes <lopes.larissalopes@gmail.com>
2019-06-20 01:38:54 -0300

    doc

--- s_aes/cipher_function.py
+++ s_aes/cipher_function.py
@@ -10,6 +10,17 @@ from s_aes.utils import distribute_array
 
 
 def cipher(input_text, input_key, rounds_range, rounds_function):
+    """
+    Criptografa ou descriptografa o texto inserido de acordo com os parametros
+
+    :param input_text: texto a ser criptografado ou descriptografado
+    :param input_key: chave usada para criptografar ou descriptografar
+    :param rounds_range: define o número de rodadas.
+        Use ROUNDS_RANGE_CIPHER para criptografar e ROUNDS_RANGE_DECIPHER para desciptografar
+    :param rounds_function: define se a função será de criptografia ou descriptografia.
+        Use cipher_rounds para criptografar e decipher_rounds para desciptografar
+    :return: texto criptografado ou descriptografado respectivamente
+    """
     key_array = key_expansion(key_format(input_key))
     state_array = text_to_state(input_text)
     cipher_text = []
@@ -20,27 +31,49 @@ def cipher(input_text, input_key, rounds_range, rounds_function):
     return int("".join(cipher_text), 2)
 
 
-def cipher_rounds(r, key_array, state):
+def cipher_rounds(r, word_array, state):
+    """
+    Executa as rodadas de criptografia
+
+    :param r: número da rodada
+    :param word_array: array contendo todas as palavras que geram as chaves
+    :param state: matriz estado
+    :return: matriz estado criptografada
+    """
     if r > 0:
         state = substitute_sbox(state, S_BOX)
         state = shift_row(state)
     if 0 < r < (N_ROUNDS - 1):
         state = mix_column(state, TRANSFORMATION_MATRIX)
-    state = add_round_key(r, state, key_array)
+    state = add_round_key(r, state, word_array)
     return state
 
 
-def decipher_rounds(r, key_array, state):
+def decipher_rounds(r, word_array, state):
+    """
+    Executa as rodadas de descriptografia
+
+    :param r: número da rodada
+    :param word_array: array contendo todas as palavras que geram as chaves
+    :param state: matriz estado
+    :return: matriz estado descriptografada
+    """
     if r < (N_ROUNDS - 1):
         state = shift_row(state)
         state = substitute_sbox(state, S_BOX_I)
-    state = add_round_key(r, state, key_array)
+    state = add_round_key(r, state, word_array)
     if 0 < r < (N_ROUNDS - 1):
         state = mix_column(state, TRANSFORMATION_MATRIX_INVERSE)
     return state
 
 
 def text_to_state(plain_text):
+    """
+    Formata o texto de entrada em uma lista de matrizes estado
+
+    :param plain_text: texto de entrada
+    :return: lista de matrizes estado
+    """
     if isinstance(plain_text, int):
         state_array = []
         binary_text = np.binary_repr(plain_text)
@@ -54,15 +87,28 @@ def text_to_state(plain_text):
         raise Exception("O texto deve conter apenas números")
 
 
-def extract_block(binary_text, part):
+def extract_block(text, part):
+    """
+    Extrai blocos do texto
+
+    :param text: texto de entrada
+    :param part: tamanho do bloco a ser extraído
+    :return: texto estraído
+    """
     if part - BLOCK_SIZE < 0:
-        text = binary_text[:part].zfill(BLOCK_SIZE)
+        text = text[:part].zfill(BLOCK_SIZE)
     else:
-        text = binary_text[part - BLOCK_SIZE:part]
+        text = text[part - BLOCK_SIZE:part]
     return text
 
 
 def state_to_text(state):
+    """
+    Converte a matriz estado em texto plano
+
+    :param state: matriz estado
+    :return: texto plano
+    """
     temp_nibble = state[2]
     state[2] = state[1]
     state[1] = temp_nibble
--- s_aes/key_expansion_function.py
+++ s_aes/key_expansion_function.py
@@ -5,6 +5,14 @@ from s_aes.substitute_sbox_function import substitute_sbox
 
 
 def key_expansion(key):
+    """
+    Expande a chave de entrada em uma lista de palavras, Para isso faz um
+    XOR bit a bit entre a palavra e a palavra anterior. Porém, se o índice
+    da palavra for um múltiplo de 2 são realizadas funções adicionais.
+
+    :param key: chave de entrada
+    :return: lista de palavras
+    """
     word_0 = key[:WORD_SIZE]
     word_1 = key[WORD_SIZE:WORD_SIZE * 2]
     word_array = [word_0, word_1]
@@ -18,6 +26,15 @@ def key_expansion(key):
 
 
 def complex_function(word, round_constant):
+    """
+    1. Efetua um deslocamento de linha na palavra
+    2. Faz substituição na palavra usando a caixa-S
+    3. Com o resultado das operações anteriores, faz um XOR bit a bit com a constante da rodada
+
+    :param word: palavra
+    :param round_constant: constante da rodada
+    :return: retorna um número inteiro
+    """
     nibble_0 = word[:NIBBLE_SIZE]
     nibble_1 = word[NIBBLE_SIZE:]
     shift_nibble = [nibble_1, nibble_0]
@@ -26,23 +43,44 @@ def complex_function(word, round_constant):
     return np.bitwise_xor(int(word, 2), int(round_constant, 2))
 
 
-def set_previous_word(i, previous_word):
+def set_previous_word(i, word):
+    """
+    define a palavra anterior no caso de índice múltiplo de 2
+
+    :param i: índice
+    :param word: palavra
+    :return: palavra
+    """
     round_constant = ROUND_CONSTANT_ARRAY[(i // 2) - 1]
-    previous_word = complex_function(previous_word, round_constant)
-    previous_word = np.binary_repr(previous_word)
-    return previous_word
+    word = complex_function(word, round_constant)
+    word = np.binary_repr(word)
+    return word
 
 
-def set_word_array(i, temp, word_array):
+def set_word_array(i, word, word_array):
+    """
+    Adiciona as novas palavras na lista
+
+    :param i: índice
+    :param word: palavra
+    :param word_array: lista de palavras
+    :return: lista de palavras
+    """
     previous = int(word_array[i - 2], 2)
-    temp_word = int(temp, 2)
-    word = np.bitwise_xor(previous, temp_word)
-    bynary_word = np.binary_repr(word, WORD_SIZE)
-    word_array.append(bynary_word)
+    int_word = int(word, 2)
+    new_word = np.bitwise_xor(previous, int_word)
+    bin_new_word = np.binary_repr(new_word, WORD_SIZE)
+    word_array.append(bin_new_word)
     return word_array
 
 
 def key_format(key):
+    """
+    Verifica inconsistências na chave inserida
+
+    :param key: chave de entrada
+    :return: chave
+    """
     if isinstance(key, int):
         key = np.binary_repr(key, KEY_SIZE)
         if len(key) > KEY_SIZE:
