5c6b9885ff69698be764726e51125c5c33587790
Larissa Lopes <lopes.larissalopes@gmail.com>
2019-06-20 23:37:06 -0300

    substitute nibble doc

--- s_aes/substitute_sbox.py
+++ s_aes/substitute_sbox.py
@@ -2,6 +2,7 @@
 def substitute_sbox(state, sbox):
     """
     Substituição dos nibbles de acordo com a caixa-S
+
     :param state: vetor estado
     :param sbox: caixa-S. Use S_BOX para caixa-S e S_BOX_I para caixa-S inversa
     :return: vetor estado
